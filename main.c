4ddbf8bd105e6a674785a358d30cb96e4138221b
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-16 12:47:00 +0100

    Update submodule and add main function using it

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "square.h"
+
+int main(int argc, char* argv[]) {
+  int n = 2;
+  if (argc > 1) {
+    n = atoi(argv[1]);
+  }
+  printf("The square of %d is %d\n", n, SQUARE(n)); 
+  return 0;
+}
+
