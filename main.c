6e7fc41f714aaf21239e7c3aa977c260767b97f4
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-17 15:21:39 +0100

    Update main.c on feature branch

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
   if (argc > 1) {
     n = atoi(argv[1]);
   }
-  printf("The square of %d is %d\n", n, SQUARE(n)); 
+  printf("The square of %d is %d.\n", n, SQUARE(n)); 
   return 0;
 }
 
