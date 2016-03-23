51bc885f463c088467d6fb3280cd22b09c105958
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-23 08:59:23 +0100

    Document pullall alias

--- set-aliases.sh
+++ set-aliases.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ## Set Git aliases (local to this repository)
 
+# Pull all remote changes to the super repository
+# and all submodules.
+# Gotcha: 'git submodule update' will silently overwrite any local changes
+# within a submodule.
 git config alias.pullall "!f() { git pull && \
     git submodule update --init --recursive && \
     git submodule foreach git pull origin master; \
