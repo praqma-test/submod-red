efb40d0324223c158e6494351ff94a68ab31283d
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-23 09:17:54 +0100

    Update git aliases

--- set-aliases.sh
+++ set-aliases.sh
@@ -9,3 +9,15 @@ git config alias.pullall "!f() { git pull && \
     git submodule update --init --recursive && \
     git submodule foreach git pull origin master; \
   }; f"
+
+# Show the status of the super repository and all submodules.
+git config alias.statusall "!f() { git status && \
+    git submodule foreach git status; \
+  }; f"
+
+# Commit in submodules and the super repository.
+# Arguments: The commit arguments, for example: -m 'commit message'
+git config alias.commitall "!f() { \
+    git submodule foreach git commit $@
+    git commit $@
+  }; f"
